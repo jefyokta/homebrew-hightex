732ee1c236106cbc0508555e08ab11ec521d94ce
Jepi Oktamipa <jefyokta50@gmail.com>
2026-09-22 00:17:53 +0700

    dsada

--- Formula/hightex@0.7.0.rb
+++ Formula/hightex@0.7.0.rb
@@ -9,7 +9,7 @@ class HightexAT070 < Formula
   end
 
   on_macos do
-    url "https://github.com/jefyokta/hightex-desktop/releases/download/v#{version}/HighTex-Mac-0.7.0-arm64.zip"
+    url "https://github.com/jefyokta/hightex-desktop/releases/download/v#{version}/HighTex-Mac-0.7.0.zip"
     sha256 "MAC_ZIP_SHA256"
   end
 
