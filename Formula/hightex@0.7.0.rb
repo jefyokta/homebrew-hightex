0c9eeb8ecd1bd04d2e0c191791b67496483d803e
Jepi Oktamipa <jefyokta50@gmail.com>
2026-09-22 00:12:43 +0700

    dsada

--- Formula/hightex@0.7.0.rb
+++ Formula/hightex@0.7.0.rb
@@ -9,8 +9,8 @@ class HightexAT070 < Formula
   end
 
   on_macos do
-    url "https://github.com/jefyokta/hightex-desktop/releases/download/v#{version}/HighTex-Mac-0.7.0-Installer.dmg"
-    sha256 "9f151c1561adb6d7d6237a4c36e2e23be66a7dc4ca1d014060266645c78bdc8c"
+    url "https://github.com/jefyokta/hightex-desktop/releases/download/v#{version}/HighTex-Mac-0.7.0-arm64.zip"
+    sha256 "MAC_ZIP_SHA256"
   end
 
   on_linux do
@@ -22,35 +22,18 @@ class HightexAT070 < Formula
 
   def install
     if OS.mac?
-      mountpoint = buildpath/"mnt"
-      mkdir mountpoint
+      prefix.install "HighTex.app"
 
-      system "/usr/bin/hdiutil", "attach",
-             "-nobrowse",
-             "-readonly",
-             "-mountpoint", mountpoint,
-             cached_download
-
-      begin
-        prefix.install mountpoint/"HighTex.app"
-
-        system "/usr/bin/xattr",
-               "-cr",
-               prefix/"HighTex.app"
-      ensure
-        system "/usr/bin/hdiutil", "detach", mountpoint
-      end
+      system "/usr/bin/xattr",
+             "-cr",
+             prefix/"HighTex.app"
     else
       libexec.install "HighTex-Linux-0.7.0.AppImage" => "HighTex.AppImage"
       chmod 0755, libexec/"HighTex.AppImage"
     end
-
-    bin.install "../bin/hightex"
   end
 
   test do
-    assert_predicate bin/"hightex", :executable?
-
     if OS.mac?
       assert_predicate prefix/"HighTex.app", :directory?
     else
