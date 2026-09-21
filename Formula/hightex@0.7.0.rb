a789c66328c23b6e43aae859c1db8579a74c6b27
Jepi Oktamipa <jefyokta50@gmail.com>
2026-09-21 23:58:59 +0700

    update! v0.7+ only

--- Formula/hightex@0.7.0.rb
+++ Formula/hightex@0.7.0.rb
@@ -1,4 +1,4 @@
-class HightexAT070< Formula
+class HightexAT070 < Formula
   desc "Desktop document editor for academic writing"
   homepage "https://github.com/jefyokta/hightex-desktop"
   version "0.7.0"
@@ -8,30 +8,59 @@ class HightexAT070< Formula
     skip "pinned version"
   end
 
+  on_macos do
+    url "https://github.com/jefyokta/hightex-desktop/releases/download/v#{version}/HighTex-Mac-0.7.0-Installer.dmg"
+    sha256 "9f151c1561adb6d7d6237a4c36e2e23be66a7dc4ca1d014060266645c78bdc8c"
+  end
+
   on_linux do
     url "https://github.com/jefyokta/hightex-desktop/releases/download/v#{version}/HighTex-Linux-0.7.0.AppImage"
     sha256 "658a92cac91d2c14f2621651029a4d216f365327b5efbc3e66a101673ce9db43"
   end
 
-  on_macos do
-    def install
-      odie "On macOS, install via Cask instead:\n  brew install --cask hightex@0.7.0"
-    end
-  end
-
   keg_only :versioned_formula
 
   def install
-    libexec.install "HighTex-Linux-0.7.0.AppImage" => "HighTex.AppImage"
-    chmod 0755, libexec/"HighTex.AppImage"
-    (libexec/".hightex-version").write version.to_s
+    if OS.mac?
+      mountpoint = buildpath/"mnt"
+      mkdir mountpoint
 
-    bin.install buildpath/"bin/hightex" => "hightex@0.7.0"
-    chmod 0755, bin/"hightex@0.7.0"
+      system "/usr/bin/hdiutil", "attach",
+             "-nobrowse",
+             "-readonly",
+             "-mountpoint", mountpoint,
+             cached_download
+
+      begin
+        prefix.install mountpoint/"HighTex.app"
+
+        system "/usr/bin/xattr",
+               "-cr",
+               prefix/"HighTex.app"
+
+        (bin/"hightex@0.7.0").write <<~SH
+          #!/bin/sh
+          exec "#{prefix}/HighTex.app/Contents/MacOS/HighTex" "$@"
+        SH
+
+        chmod 0755, bin/"hightex@0.7.0"
+      ensure
+        system "/usr/bin/hdiutil", "detach", mountpoint
+      end
+    else
+      libexec.install "HighTex-Linux-0.7.0.AppImage" => "HighTex.AppImage"
+      chmod 0755, libexec/"HighTex.AppImage"
+
+      (bin/"hightex@0.7.0").write <<~SH
+        #!/bin/sh
+        exec "#{libexec}/HighTex.AppImage" "$@"
+      SH
+
+      chmod 0755, bin/"hightex@0.7.0"
+    end
   end
 
   test do
     assert_predicate bin/"hightex@0.7.0", :executable?
-    assert_predicate libexec/"HighTex.AppImage", :executable?
   end
 end
